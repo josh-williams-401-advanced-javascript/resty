d02e40b47fbc859caaefaca3243f92eea718e4a1
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-09-14 17:08:33 -0700

    finish class 26

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="App-footer">
+      <p>&copy; Josh Williams 2020</p>
+    </footer>
+  )
+}
+
+export default Footer;
