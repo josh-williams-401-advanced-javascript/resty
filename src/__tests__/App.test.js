9510eeb29727ab9580ba9074a767ad6683fbec1b
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-09-15 18:25:06 -0700

    add three tests

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import App from '../App'
+import Results from '../components/results'
+
+it('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const header = getByText(/resty/i);
+  expect(header).toBeInTheDocument();
+});
+
+it('will not render results section before there are results', async () =>{
+  const { container } = render(<Results data={''} resultsIn={null}/>);
+  expect(container).toBeEmpty();
+})
+
+it('should render json data when data is passed to results', () => {
+  const people = {
+    'Luke Skywalker': 'http://swapi.dev/api/people/1/',
+    'Darth Vader': 'http://swapi.dev/api/people/4/',
+  }
+  const { getByText } = render(<Results data={people} resultsIn={'results'}/>);
+  const newHeading = screen.getByRole('heading');
+  const luke = getByText('Luke Skywalker');
+  const darthUrl = getByText('http://swapi.dev/api/people/4/');
+  expect(newHeading).toHaveTextContent('Results');
+  expect(luke).toBeInTheDocument();
+  expect(darthUrl).toBeInTheDocument();
+});
+
+
--- src/components/form.js
+++ src/components/form.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
 class Form extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       method: '',
       url: '',
-
     }
   }
 
   setMethod = e => this.setState({ method: e.target.value })
   setUrl = e => this.setState({ url: e.target.value });
-
-
 
   sendInput = async () => {
     let rawData = await fetch(this.state.url);
@@ -25,7 +23,7 @@ class Form extends React.Component {
       <section className="form">
         <fieldset>
           <legend>URL</legend>
-          <input placeholder="http://" onChange={this.setUrl} />
+          <input data-testid="urlInput" placeholder="http://" onChange={this.setUrl} />
         </fieldset>
         <fieldset>
           <legend>Method</legend>
@@ -33,7 +31,7 @@ class Form extends React.Component {
 
             <input type="radio" value="GET" name="method" onChange={this.setMethod} />
             <label htmlFor="GET">GET</label>
-            <input type="radio" value="POST" name="method" onChange={this.setMethod} />
+            <input data-testid="methodInput" type="radio" value="POST" name="method" onChange={this.setMethod} />
             <label htmlFor="POST">POST</label>
             <input type="radio" value="PUT" name="method" onChange={this.setMethod} />
             <label htmlFor="PUT">PUT</label>
@@ -41,7 +39,7 @@ class Form extends React.Component {
             <label htmlFor="DELETE">DELETE</label>
           </div>
         </fieldset>
-        <button onClick={this.sendInput}>Go</button>
+        <button data-testid="submit" onClick={this.sendInput}>Go</button>
       </section>
 
     </>
