0fd7198960d833bed33c96b60f05ca5c95ecdd40
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-09-21 10:09:46 -0700

    Update form.js

--- src/components/form.js
+++ src/components/form.js
@@ -3,10 +3,10 @@ import { Route } from 'react-router-dom';
 // import Editor from 'react-simple-code-editor';
 // import Prism from 'prismjs';
 // import { highlight, languages } from 'prismjs/components/prism-core';
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
-import '../style/form.scss'
-import '../style/base.scss'
+// import 'prismjs/components/prism-clike';
+// import 'prismjs/components/prism-javascript';
+// import '../style/form.scss'
+// import '../style/base.scss'
 
 class Form extends React.Component {
 
